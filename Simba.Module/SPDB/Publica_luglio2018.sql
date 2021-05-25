21ff3812a38e22b18beeb8d4810aaa3eaf7c26cf
Federico Pranio <federico.pranio@nexusat.it>
2021-05-25 16:02:40 +0200

    Eliminazione cose inutili

--- Simba.Module/SPDB/Publica_luglio2018.sql
+++
@@ -1,6 +0,0 @@
---pubblicazioni da fare bluglio 2018
-
-select t.*, t.rowid from MPREGPIANIFICAZIONE t
-update  MPREGPIANIFICAZIONE t set t.tipoaggregazioneregrdl = 0;
------------------------------------------------------------------
-
