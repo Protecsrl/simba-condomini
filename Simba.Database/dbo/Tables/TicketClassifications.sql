729ba206c3455f05e9526b54f3ea18da8a064892
DESKTOP-JM24IAK\Andrea <anovelli>
2021-06-20 16:43:23 +0200

    fix campo tabella

--- Simba.Database/dbo/Tables/TicketClassifications.sql
+++ Simba.Database/dbo/Tables/TicketClassifications.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[TicketClassifications] (
+    [Oid]              INT      IDENTITY (1, 1) NOT NULL,
     [IdTicket]         INT      NOT NULL,
     [IdClassification] INT      NOT NULL,
     [DateInser]        DATETIME NULL,
     [UserId]           INT      NULL,
-    [Oid]              INT      IDENTITY (1, 1) NOT NULL,
     CONSTRAINT [PK_TicketClassifications_1] PRIMARY KEY CLUSTERED ([Oid] ASC),
     CONSTRAINT [FK_TicketClassifications_Ticket] FOREIGN KEY ([IdTicket]) REFERENCES [dbo].[Ticket] ([Oid]),
     CONSTRAINT [FK_TicketClassifications_TicketClassification] FOREIGN KEY ([IdClassification]) REFERENCES [dbo].[TicketClassification] ([Oid]),
